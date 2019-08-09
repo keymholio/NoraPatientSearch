4a7d3fcc078fe2b838dac3035a835e01c4fce645
Andrew K <andrew.keym@gmail.com>
2019-08-09 13:45:08 -0400

    Adding additional pages

--- js/main.js
+++ js/main.js
@@ -5,7 +5,21 @@ window.onload = function() {
     document.addEventListener('tizenhwkey', function(e) {
         if (e.keyName == "back")
             try {
-                tizen.application.getCurrentApplication().exit();
+                //tizen.application.getCurrentApplication().exit();
+            		window.history.back()
             } catch (ignore) {}
     });
 };
+
+$(function() {
+	$.ajax({
+        url: "https://neti-galaxywatch-nora2.azurewebsites.net/api/GetPatientResource",
+        headers: { "x-functions-key": "2ZbTI46j2QTBuo3t11IvQPGfF2cBIuvHt1hnXICWkoaqMtbQKLQHkw==" },
+
+        // type: "POST",
+        success: function(data) {
+        		$('.result').html(data.patientName);  
+        }
+     });
+});
+
